f6c8cd7795553eb90d8c87eb3b0adff19b292181
MP-Alumno <MP-Alumno@duoc.acad>
2026-06-09 14:12:50 -0400

    feat: Implementa BaseEntity para manejar auditoria

--- backend/src/main/java/com/taller/actividad/ActividadApplication.java
+++ backend/src/main/java/com/taller/actividad/ActividadApplication.java
@@ -2,8 +2,10 @@ package com.taller.actividad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ActividadApplication {
 
 	public static void main(String[] args) {
