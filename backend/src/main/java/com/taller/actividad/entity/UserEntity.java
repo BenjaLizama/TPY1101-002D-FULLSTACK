e769ceab68cbfbce4b7f3aaebdec026a9bb33f1f
MP-Alumno <MP-Alumno@duoc.acad>
2026-06-09 14:16:16 -0400

    feat: Implementa clase usuarios

---
+++ backend/src/main/java/com/taller/actividad/entity/UserEntity.java
@@ -0,0 +1,30 @@
+package com.taller.actividad.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "USUARIOS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class UserEntity extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Integer edad;
+
+}
